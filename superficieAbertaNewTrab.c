a8f3e1a212623c76fda62fcf2f0bc6e6c097a8a6
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2019-02-09 11:45:19 -0200

    Adiciona float nos parametros

--- superficieAbertaNewTrab.c
+++ superficieAbertaNewTrab.c
@@ -94,7 +94,7 @@ matriz *pcPatch = NULL;    // matriz de pontos para um patch
 matriz *ptsPatch = NULL;   // matriiz de PONTOS na superficie PATCH calclulados
 
 void DisenaSuperficie(void);
-float CalculaAngulo();
+float CalculaAngulo(float);
 
 // ----------------------------------------------------------------------------
 // OBSERVACAO 1:
